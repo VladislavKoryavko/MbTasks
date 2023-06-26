c398974c77ce6a8b878ffe18e291db4e42373f61
VladislavKoryavko <vladislav.inco@gmail.com>
2023-06-26 13:52:42 +0300

    Created project and tables, added test data

---
+++ Database/dbo/Tables/ProductCategories.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductCategories]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductId] INT NOT NULL,
+	[CategoryId] INT NOT NULL
+
+	CONSTRAINT FK_ProductCategories_Products FOREIGN KEY ([ProductId]) REFERENCES Products ([Id])
+	CONSTRAINT FK_ProductCategories_Categories FOREIGN KEY ([CategoryId]) REFERENCES Categories ([Id])
+)
