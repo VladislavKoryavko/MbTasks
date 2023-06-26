55172190826dcaeb9b723463646251292e993bfe
VladislavKoryavko <vladislav.inco@gmail.com>
2023-06-26 03:50:56 +0300

    Added unit tests for Circle class

---
+++ MindShape.Tests/Shapes/CircleTests/CircleTests.GetArea.cs
@@ -0,0 +1,19 @@
+﻿using MindShape.Lib.Shapes;
+
+namespace MindShape.Tests.Shapes.CircleTests;
+
+public partial class CircleTests
+{
+    [Fact]
+    public void GetArea()
+    {
+        // Arrange
+        var circle = new Circle(10);
+
+        // Act
+        var result = circle.GetArea();
+
+        // Assert
+        Assert.Equal(314.15926535897932384626433832795, result);
+    }
+}
