2b6c60bf401723935b42419f04d40402c4c08523
VladislavKoryavko <vladislav.inco@gmail.com>
2023-06-26 15:34:33 +0300

    Added description for classes, fields, properties and methods

--- MindShape.Lib/Shapes/Circle.cs
+++ MindShape.Lib/Shapes/Circle.cs
@@ -1,9 +1,18 @@
 ﻿namespace MindShape.Lib.Shapes;
 
+/// <summary>
+/// Класс круг, наследник базового класса фигура
+/// </summary>
 public class Circle : Shape
 {
+    /// <summary>
+    /// Поле хранящее радиус круга
+    /// </summary>
     private double _radius;
 
+    /// <summary>
+    /// Свойство отвечающее за работу с полем радиуса
+    /// </summary>
     public double Radius
     {
         get => _radius;
@@ -18,18 +27,35 @@ public class Circle : Shape
         }
     }
 
+    /// <summary>
+    /// Публичный конструктор для круга
+    /// </summary>
+    /// <param name="radius"> Радиус создаваемого круга </param>
     public Circle(double radius)
     {
         Radius = radius;
     }
 
+    /// <summary>
+    /// Метод рассчитывающий длину окружности по радиусу круга
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
     private static double GetCircumferenceByRadius(double radius) => 2 * Math.PI * radius;
 
+    /// <summary>
+    /// Метод предоставляющий периметр круга
+    /// </summary>
+    /// <returns></returns>
     public override double GetPerimeter()
     {
         return GetCircumferenceByRadius(Radius);
     }
 
+    /// <summary>
+    /// Метод возвращающий площадь круга
+    /// </summary>
+    /// <returns></returns>
     public override double GetArea()
     {
         return Math.PI * Math.Pow(Radius, 2);
--- MindShape.Lib/Shapes/Triangle.cs
+++ MindShape.Lib/Shapes/Triangle.cs
@@ -1,19 +1,41 @@
 ﻿namespace MindShape.Lib.Shapes;
 
+/// <summary>
+/// Класс треугольник, наследник класса полигон
+/// </summary>
 public class Triangle : Polygon
 {
+    /// <summary>
+    /// Допустимое отклонение в операциях сравнения чисел с плавающей точкой
+    /// </summary>
     private const double ToleranceComparison = 1e-6;
 
+    /// <summary>
+    /// Публичный конструктор
+    /// </summary>
+    /// <param name="sides"> Список сторон треугольника </param>
     public Triangle(List<double> sides) : base(sides) { }
 
+    /// <summary>
+    /// Метод возвращающий количество сторон треугольника 
+    /// </summary>
+    /// <returns></returns>
     protected override byte GetNumberSides() => 3;
 
+    /// <summary>
+    /// Метод возвращающий площадь треугольника
+    /// </summary>
+    /// <returns></returns>
     public override double GetArea()
     {
         var halfPerimeter = GetPerimeter() / 2;
         return Math.Sqrt(halfPerimeter * Sides.Aggregate((a, b) => a * (b - halfPerimeter)));
     }
 
+    /// <summary>
+    /// Данный метод определяет является ли треугольник прямоугольным
+    /// </summary>
+    /// <returns></returns>
     public bool IsRectangular()
     {
         return Math.Abs(Math.Pow(Sides.Max(), 2) * 2 - Sides.Select(s => s * s).Sum()) < ToleranceComparison;
